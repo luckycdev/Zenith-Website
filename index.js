6f4dbba0d45ca32499baa25e69e2ca4d3dad8b97
Vincent <luckzstuff@gmail.com>
2025-06-14 17:26:35 -0500

    add reloading to server list

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function getServerList(){
-fetch("https://corsproxy.io/?https://master.gettingoverit.mp/list")
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById("servers").textContent = data;
-  });
+    document.getElementById("servers").textContent = "Loading...";
+    fetch("https://corsproxy.io/?https://master.gettingoverit.mp/list")
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById("servers").textContent = data;
+      });
 }
